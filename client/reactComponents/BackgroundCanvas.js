0f5cac510dedab940f8eecd939f982fb3b4ba246
Ian deBoisblanc <iandeboisblanc@gmail.com>
2016-07-06 20:35:27 -0700

    Have bubbles that rise

--- client/reactComponents/BackgroundCanvas.js
+++ client/reactComponents/BackgroundCanvas.js
@@ -10,43 +10,63 @@ class BackgroundCanvas extends React.Component {
   constructor(props) {
     super(props);
     this.width = window.innerWidth;
+    this.lastWidth = this.width;
     this.height = window.innerHeight;
-    this.bubbles = [];
-    for(var i = 0; i < 100; i++) {
-      this.bubbles.push({
-        id: i,
-        radius: Math.random() * 7 + 3,
-        x: Math.random() * this.width,
-        y: Math.random() * this.height,
-        rate: Math.random() * 10 + 10
-      });
-    }
+    this.bubbles = createBubbles(this.width / 5);
   }
 
   componentDidMount () {
     d3.select(window)
       .on('resize', () => {
+        this.lastWidth = this.width;
         this.width = window.innerWidth;
         this.height = window.innerHeight;
         d3.select('svg')
           .attr('width', this.width)
           .attr('height', this.height);
+        // if(this.width > this.lastWidth) {
+        //   this.bubbles = [...this.bubbles, ...createBubbles((this.width - this.lastWidth) / 5, this.lastWidth, this.width)];
+        //   d3.selectAll('circle')
+        //     .data(this.bubbles, d => d.id)
+        //     .enter()
+        //     .append('circle')
+        //     .attr('r', d => d.radius)
+        //     .attr('cx', d => d.x * this.width)
+        //     .attr('cy', d => d.y * this.height);
+        // }
       });
+    
+    this.animateBubbles = this.animateBubbles.bind(this);
+    setInterval(this.animateBubbles, 10);
+  }
+
+  animateBubbles() {
     d3.select('svg').selectAll('circle')
       .data(this.bubbles, d => d.id)
       .enter()
       .append('circle')
+      .attr('class', 'bubble')
       .attr('r', d => d.radius)
-      .attr('cx', d => d.x)
-      .attr('cy', d => d.y);
-    // animateBubbles = animateBubbles.bind(this);
-    // setInterval(animateBubbles, 10);
+      .attr('cx', d => d.x * this.width)
+      .attr('cy', d => d.y * this.height);
+
+    d3.selectAll('circle')
+      .data(this.bubbles, d => d.id)
+      .attr('cx', d => d.x * this.width)
+      .attr('cy', d => {
+        if(d.y < 0.01) {
+          d.y = 1.01;
+        } else {
+          d.y -= d.rate;
+        }
+        return d.y * this.height;
+      });
   }
 
   render () {
     return (
       <div className='backgroundCanvas'>
-        <svg width={window.innerWidth} height={window.innerHeight} />
+        <svg width={this.width} height={this.height} />
       </div>
     );
   }
@@ -55,8 +75,18 @@ class BackgroundCanvas extends React.Component {
 
 module.exports = BackgroundCanvas;
 
-// function animateBubbles() {
-//   this.bubbles.forEach((bubble) => {
-
-//   })
-// }
+function createBubbles(n, xMin, xMax) {
+  var bubbles = [];
+  xMax = xMax / xMax || 1;
+  xMin = xMin / xMax || 0;
+  for(var i = 0; i < n; i++) {
+    bubbles.push({
+      id: Math.random() * 1000000000,
+      radius: Math.random() * 7 + 3,
+      x: Math.random() * (xMax - xMin) + xMin,
+      y: 1 + Math.random(),
+      rate: (Math.random() * 15 + 8)  / 50000
+    });
+  }
+  return bubbles;
+}
