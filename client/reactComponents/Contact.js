1188cf5bc177c00ff28d6e90a99fd8c0fe65f842
Ian deBoisblanc <iandeboisblanc@gmail.com>
2016-07-08 01:20:35 -0700

    Add clipboard funcitonality for email

--- client/reactComponents/Contact.js
+++ client/reactComponents/Contact.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import Clipboard from 'clipboard';
 
 const Contact = (props) => {
+  new Clipboard('.btn');
   return (
     <div key='contact' className='contact'>
+      <div>
+        <h4> iandeboisblanc@gmail.com</h4>
+        <button className='btn' data-clipboard-text='iandeboisblanc@gmail.com'> Copy </button>
+      </div>
+      <div> icons for...
+        <a href='http://www.Github.com/iandeboisblanc' target='_blank'> Github </a>
+        <a href='http://www.LinkedIn.com/in/iandeboisblanc' target='_blank'> LinkedIn </a>
+      </div>
     </div>
   )
 }
