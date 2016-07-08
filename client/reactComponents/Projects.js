afcffb81c53874e557ab1bd5a0148893b1a611c2
Ian deBoisblanc <iandeboisblanc@gmail.com>
2016-07-08 01:36:46 -0700

    Mock out more projects

--- client/reactComponents/Projects.js
+++ client/reactComponents/Projects.js
@@ -12,7 +12,58 @@ const Projects = (props) => {
           Potentially a bit more
         </p>
         <p>
-          <a href='/evolution'> click here </a>
+          <a href='/evolution' target='_blank'> Visit </a>
+          <a href='https://github.com/iandeboisblanc/evolution' target='_blank'> GH (Icon) </a>
+        </p>
+      </div>
+      <div className='project'>
+        <h2> VR WikiMuseum </h2>
+        <p>
+          Some text about VR WikiMuseum
+        </p>
+        <p>
+          Potentially a bit more
+        </p>
+        <p>
+          <a href='http://www.vrwikimuseum.com' target='_blank'> Visit </a>
+          <a href='https://github.com/iandeboisblanc/VRwikiMuseum' target='_blank'> GH (Icon) </a>
+        </p>
+      </div>
+      <div className='project'>
+        <h2> BubbleTanks 3D </h2>
+        <p>
+          Some text about BubbleTanks 3D
+        </p>
+        <p>
+          Potentially a bit more
+        </p>
+        <p>
+          <a href='http://www.bubbletanks.biz' target='_blank'> Play </a>
+          <a href='https://github.com/ourvrisrealerthanyours/tanks' target='_blank'> GH (Icon) </a>
+        </p>
+      </div>
+      <div className='project'>
+        <h2> Thumbroll </h2>
+        <p>
+          Some text about Thumbroll
+        </p>
+        <p>
+          Potentially a bit more
+        </p>
+        <p>
+          <a href='http://www.thumbroll.xyz' target='_blank'> Link?? </a>
+        </p>
+      </div>
+      <div className='project'>
+        <h2> Web Gobbler </h2>
+        <p>
+          Some text about Gobbler
+        </p>
+        <p>
+          Potentially a bit more
+        </p>
+        <p>
+          <a href='http://www.gobblerweb.com' target='_blank'> Visit </a>
         </p>
       </div>
     </div>
@@ -20,3 +71,5 @@ const Projects = (props) => {
 }
 
 module.exports = Projects;
+
+// could have project component that takes object with title, paragraphs, link, and image
