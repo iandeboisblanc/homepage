bec2ac88eb662979cf2c3139bab47afdc49a16d7
Ian deBoisblanc <iandeboisblanc@gmail.com>
2016-07-12 18:22:25 -0700

    Edit about

--- client/reactComponents/About.js
+++ client/reactComponents/About.js
@@ -4,21 +4,28 @@ const About = (props) => {
   return (
     <div key='about' className='about'>
       <p>
-        I'm a full stack software engineer who loves to build mind-blowing apps for web,
-        mobile, and virtual reality. I write code that is fun to play with, both for developers
-        and users, because it's clean, intuitive, and friendly.
+        I am a full-stack software engineer specializing in designing and building unique, interactive
+        apps which are simulataneously engaging and intuitive. I enjoy staying up to date on current technologies,
+        leading me to develop application for web, mobile, and virtual reality.
       </p>
       <p>
-        I've worked extensively on both the front and back ends of several apps, created
-        RESTful APIs, designed complex database schemas, and engineered responsive user interfaces.
+        I have worked extensively across the stack, from crafting fluid frontend interfaces,
+        to wiring web sockets for real-time communication, to designing and implementing SQL and No-SQL database schemas,
+        to deploying sites hosted within Docker containers.
       </p>
       <p>
-        The technologies I know and love include A-Frame VR, HTML5, CSS3, React, React Native,
-        Node, Angular, jQuery, Postgres, Sequelize, SQL, Backbone, DigitalOcean, git, MongoDB,
-        Mongoose, and ES6.
+        I am well-versed in numerous web technologies including React, React Native, Angular, Backbone, AframeVR,
+        JQuery, HTML5, CSS3, ES6, Node, PostreSQL, MongoDB, Docker, and Git.
       </p>
     </div>
   )
 }
 
 module.exports = About;
+
+
+
+
+
+
+
